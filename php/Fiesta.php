21e1f13d67aa0005e065e774228139a793568791
Levi Durfee <levi.durfee@gmail.com>
2017-04-07 08:05:37 -0400

    add recursive factorial method

--- php/Fiesta.php
+++ php/Fiesta.php
@@ -1,5 +1,10 @@
 <?php
 
 class Fiesta {
-	
+	public function factorial($number, $accumulator) {
+		if($number === 0) {
+			return $accumulator;
+		}
+		return $this->factorial($number - 1, $number * $accumulator);
+	}
 }
