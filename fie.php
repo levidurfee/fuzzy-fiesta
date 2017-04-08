f5235a7b30241e634d026d48d0a81590a1165e99
Levi Durfee <levi.durfee@gmail.com>
2017-04-07 23:05:22 -0400

    Add usage example for bst

---
+++ fie.php
@@ -0,0 +1,12 @@
+<?php
+
+include('php/BinaryNode.php');
+include('php/BinaryTree.php');
+
+$bst = new BinaryTree;
+$bst->insert(40);
+$bst->insert(39);
+$bst->insert(50);
+$bst->insert(49);
+
+var_dump($bst);
