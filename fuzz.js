50e948782e9a78bf23cea94260fb2b0f17e3f09a
Levi Durfee <levi.durfee@gmail.com>
2017-04-09 21:25:53 -0400

    change indents to spaces

--- fuzz.js
+++ fuzz.js
@@ -1,42 +1,42 @@
 class BubbleSort {
-	static sort(numbers) {
-		let iteration = 0;
-		let swap = false;
-		do {
-			for(let i=0; i<numbers.length; i++) {
-				if(!numbers[i + 1]) {
-					continue;
-				}
-				if(numbers[i] > numbers[i + 1]) {
-					let large = numbers[i];
-					let small = numbers[i + 1];
+    static sort(numbers) {
+        let iteration = 0;
+        let swap = false;
+        do {
+            for(let i=0; i<numbers.length; i++) {
+                if(!numbers[i + 1]) {
+                    continue;
+                }
+                if(numbers[i] > numbers[i + 1]) {
+                    let large = numbers[i];
+                    let small = numbers[i + 1];
 
-					numbers[i] = small;
-					numbers[i + 1] = large;
-					swap = true;
-				}
-			}
+                    numbers[i] = small;
+                    numbers[i + 1] = large;
+                    swap = true;
+                }
+            }
 
-			iteration++;
+            iteration++;
 
-			if(iteration >= Math.pow(numbers.length, 2)) {
-				swap = false;
-			}
-			
-		}
-		while(swap);
+            if(iteration >= Math.pow(numbers.length, 2)) {
+                swap = false;
+            }
+            
+        }
+        while(swap);
 
-		return numbers;
-	}
+        return numbers;
+    }
 }
 
 let numbers = [
-	90,
-	80,
-	10,
-	70,
-	55,
-	43,
+    90,
+    80,
+    10,
+    70,
+    55,
+    43,
 ];
 
 console.log('Before', numbers);
